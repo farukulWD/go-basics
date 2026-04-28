0aa79f5682ff5c16adaab68313560a68f2365d66
farukulWD <farukgb1999@gmail.com>
2026-04-28 21:47:14 +0600

    feat: integrate zerolog for structured logging in main.go

--- day27-logging/main.go
+++ day27-logging/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// Pretty console output for development
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
+
+	log.Info().Msg("Server starting")
+	log.Warn().Msg("This is a warning")
+	log.Error().Msg("Something went wrong")
+	log.Debug().Msg("Debug details")
 }
